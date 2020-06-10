98751c53f5549ca8a9cb3c3804b1f371d44ef71b
Lu342 <lucasfreitasv.rn@gmail.com>
2020-06-10 17:23:42 -0300

    Adicionando métodos de busca por nome e numero, além do método mostrar

--- src/br/edu/fafic/engsoft/agenda_telefonica/service/AgendaService.java
+++ src/br/edu/fafic/engsoft/agenda_telefonica/service/AgendaService.java
@@ -1,4 +1,20 @@
 package br.edu.fafic.engsoft.agenda_telefonica.service;
 
+import br.edu.fafic.engsoft.agenda_telefonica.model.ContatoModel;
+
+import java.util.ArrayList;
+
 public class AgendaService {
+
+    public String buscarContatoNome(String nome){
+        return null;
+    }
+
+    public String buscarContatoNumero(String numero){
+        return null;
+    }
+
+    public ArrayList<ContatoModel> mostrar(){
+        return null;
+    }
 }
