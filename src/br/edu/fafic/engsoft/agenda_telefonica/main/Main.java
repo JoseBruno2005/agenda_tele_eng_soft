f869c0f5356c8e4a282d382b0e2413fdddc3cb46
Erlon Jr <ejrgeek@gmail.com>
2020-06-10 18:50:32 -0300

    AgendaController implementada e sendo chamada no main | Metodos de AgendaService todos publicos

--- src/br/edu/fafic/engsoft/agenda_telefonica/main/Main.java
+++ src/br/edu/fafic/engsoft/agenda_telefonica/main/Main.java
@@ -1,7 +1,15 @@
 package br.edu.fafic.engsoft.agenda_telefonica.main;
 
+import br.edu.fafic.engsoft.agenda_telefonica.controller.AgendaController;
+import br.edu.fafic.engsoft.agenda_telefonica.service.AgendaService;
+
 public class Main {
     public static void main(String[] args) {
+        AgendaController controller = new AgendaController();
+
+        controller.remover("jose", "nome");
+
+
 
     }
 }
